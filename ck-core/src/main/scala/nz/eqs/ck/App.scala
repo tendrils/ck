531c0f10e06657c60671cead88c6998412b155ed
Alex Fulton <alex@eqs.nz>
2017-12-16 09:44:17 +1300

    remove files which no longer exist

--- ck-core/src/main/scala/nz/eqs/ck/App.scala
+++
@@ -1,5 +0,0 @@
-package nz.eqs.ck
-
-abstract class App[A: App](services: List[AppSpecificService[A,A,_]]) extends AppSpecificModule[A,A](services) {
-
-}
--- ck-core/src/main/scala/nz/eqs/ck/CKApplicationService.scala
+++
@@ -1,5 +0,0 @@
-package nz.eqs.ck
-
-abstract class CKApplicationService[A: App[A]] extends AppSpecificService[A,A,CKApplicationService[A]] {
-
-}
